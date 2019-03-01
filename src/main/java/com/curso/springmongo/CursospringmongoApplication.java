efcc6b2231528fc01f1b2ca58b6aa0691bd0fed3
Denilso Fabiano Brustelo <denilso.brustelo@gmail.com>
2019-03-01 11:42:32 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/curso/springmongo/CursospringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringmongoApplication.class, args);
+	}
+
+}
