a98712400ed60363d66c78b305c94803315d1d17
Denilso Fabiano Brustelo <denilso.brustelo@gmail.com>
2019-03-11 14:53:12 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/curso/springmongo/config/Instantiation.java
+++ src/main/java/com/curso/springmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.curso.springmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.curso.springmongo.domain.Post;
 import com.curso.springmongo.domain.User;
+import com.curso.springmongo.repository.PostRepository;
 import com.curso.springmongo.repository.UserRepository;
 
 @Configuration
@@ -14,17 +18,29 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userReposiroty;
+	
+	@Autowired
+	private PostRepository postReposiroty;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
+		
 		userReposiroty.save(Arrays.asList(maria, alex, bob));
+		postReposiroty.save(Arrays.asList(post1, post2));
 		
 	}
 	
