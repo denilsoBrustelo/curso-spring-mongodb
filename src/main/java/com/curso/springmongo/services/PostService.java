25148dcee8e134bbcfba4b33ac8650e25b206e50
Denilso Fabiano Brustelo <denilso.brustelo@gmail.com>
2019-03-12 15:01:52 -0300

    Consulta simples com query methods

--- src/main/java/com/curso/springmongo/services/PostService.java
+++ src/main/java/com/curso/springmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.curso.springmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +22,10 @@ public class PostService {
 		}
 		return post;
 	}
+	
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
